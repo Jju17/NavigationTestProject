0761c4e545d24483b632905e746d8fc68c2ca512
julien.rahier <julien.rahier@persgroep.net>
2024-06-01 14:57:07 +0200

    Initial Commit

---
+++ NavigationTestProject/NavigationTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestProjectApp.swift
+//  NavigationTestProject
+//
+//  Created by Julien Rahier on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
